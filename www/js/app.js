5e26e37ed240a691c00732225d00328844a0927a
rafaelmaki <rafamitsuom@gmail.com>
2016-10-01 14:19:18 -0300

    Novo Contato

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,11 @@
+/* global angular */
+
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var example = angular.module('starter', ['ionic', 'ngCordova'])
+var app = angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,31 +25,82 @@ var example = angular.module('starter', ['ionic', 'ngCordova'])
   });
 });
 
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('list', {
+    url: '/',
+    templateUrl: 'index.html',
+    controller : "ContatosCtrl"
+  })
+  .state('new', {
+    url: '/new',
+    templateUrl: 'new.html',
+    controller : "NewCtrl"
+  })
+  .state('edit', {
+    url: '/edit',
+    templateUrl: 'edit.html',
+    // controller : "EditCtrl"
+  })
+  
+  $urlRouterProvider.otherwise("/");
+})
 
-example.controller('ExampleController', function($scope, $cordovaContacts) {
- 
+app.controller('ContatosCtrl', function($scope, $cordovaContacts) {
+
     $scope.getContactList = function() {
-    $cordovaContacts.find({filter: ''}).then(function(result) {
-        $scope.contacts = result;
-    }, function(error) {
-        console.log("ERROR: " + error);
-    });
-}
+        $cordovaContacts.find({filter: ''}).then(function(result) {
+            $scope.contacts = result;
+        }, function(error) {
+            console.log("ERROR: " + error);
+        });
+    };
  
     $scope.createContact = function() {
-    $cordovaContacts.save({"displayName": "Steve Jobs"}).then(function(result) {
-        console.log(JSON.stringify(result));
-    }, function(error) {
-        console.log(error);
-    });
-}
+
+        // $cordovaContacts.save(
+        //     {
+        //         "displayName": "Steve Jobs", 
+        //         "name": "Steve Jobs", 
+        //         "phoneNumbers": [ {"number": "123456789"}], 
+        //         "emails": [{"email": "SteveJobs@apple.com"}]
+        //     }).then(function(result) {
+        //     console.log(JSON.stringify(result));
+        // }, function(error) {
+        //     console.log(error);
+        // });
+    };
     
-$scope.removeContact = function() {
-    $cordovaContacts.remove({"displayName": "Steve Jobs"}).then(function(result) {
-        console.log(JSON.stringify(result));
-    }, function(error) {
-        console.log(error);
-    });
-}
+    $scope.removeContact = function() {
+        $cordovaContacts.remove({"displayName": "Steve Jobs"}).then(function(result) {
+            console.log(JSON.stringify(result));
+        }, function(error) {
+            console.log(error);
+        });
+    };
+
+    $scope.editContact = function(contact) {
+
+    }
+ 
+});
+
+
+app.controller('NewCtrl', function($scope, $cordovaContacts) {
+    $scope.createContact = function(contact) {
+        
+        $cordovaContacts.save(
+            {
+                "displayName": contact.nome, 
+                "name": contact.nome, 
+                "phoneNumbers": [ {"number": contact.telefone}], 
+                "emails": [{"email": contact.email}]
+            }).then(function(result) {
+            console.log(JSON.stringify(result));
+            
+        }, function(error) {
+            console.log(error);
+        });
+    };
  
 });
